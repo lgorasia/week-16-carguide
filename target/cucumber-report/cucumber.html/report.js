853ce0c5b35d0e5f77aff490f26f1dac93e88e43
lgorasia <lalit.gorasia295@gmail.com>
2022-05-27 19:52:06 +0100

    week 16 homeword

---
+++ target/cucumber-report/cucumber.html/report.js
@@ -0,0 +1,308 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("buy.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Search functionality",
+  "description": "",
+  "id": "search-functionality",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 3,
+  "name": ": Search the buy car with model",
+  "description": "",
+  "id": "search-functionality;:-search-the-buy-car-with-model",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@smoke"
+    }
+  ]
+});
+formatter.step({
+  "line": 4,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I mouse hover on “buy+sell” tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I click ‘Search Cars’ link",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "I navigate to ‘new and used car search’ page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "I select make \"\u003cmake\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I select model \"\u003cmodel\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I select location \"\u003clocation\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I select price \"\u003cprice\u003e\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "I click on Find My Next Car tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "I should see the make \"\u003cmake\u003e\" in results",
+  "keyword": "Then "
+});
+formatter.examples({
+  "comments": [
+    {
+      "line": 14,
+      "value": "#"
+    }
+  ],
+  "line": 15,
+  "name": "",
+  "description": "",
+  "id": "search-functionality;:-search-the-buy-car-with-model;",
+  "rows": [
+    {
+      "cells": [
+        "make",
+        "model",
+        "location",
+        "price"
+      ],
+      "line": 16,
+      "id": "search-functionality;:-search-the-buy-car-with-model;;1"
+    },
+    {
+      "comments": [
+        {
+          "line": 17,
+          "value": "#      | Jeep  | Cherokee  | ACT-All      | $20,000     |"
+        }
+      ],
+      "cells": [
+        "BMW",
+        "Any Model",
+        "ACT-All",
+        "$15,000"
+      ],
+      "line": 18,
+      "id": "search-functionality;:-search-the-buy-car-with-model;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 11678525500,
+  "status": "passed"
+});
+formatter.scenario({
+  "comments": [
+    {
+      "line": 17,
+      "value": "#      | Jeep  | Cherokee  | ACT-All      | $20,000     |"
+    }
+  ],
+  "line": 18,
+  "name": ": Search the buy car with model",
+  "description": "",
+  "id": "search-functionality;:-search-the-buy-car-with-model;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 2,
+      "name": "@smoke"
+    }
+  ]
+});
+formatter.step({
+  "line": 4,
+  "name": "I am on homepage",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I mouse hover on “buy+sell” tab",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 6,
+  "name": "I click ‘Search Cars’ link",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 7,
+  "name": "I navigate to ‘new and used car search’ page",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 8,
+  "name": "I select make \"BMW\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "I select model \"Any Model\"",
+  "matchedColumns": [
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "I select location \"ACT-All\"",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "I select price \"$15,000\"",
+  "matchedColumns": [
+    3
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "I click on Find My Next Car tab",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "I should see the make \"BMW\" in results",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HomePageSteps.iAmOnHomepage()"
+});
+formatter.result({
+  "duration": 397991600,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.iMouseHoverOnBuySellTab()"
+});
+formatter.result({
+  "duration": 593471500,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.iClickSearchCarsLink()"
+});
+formatter.result({
+  "duration": 2881332100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.iNavigateToNewAndUsedCarSearchPage()"
+});
+formatter.result({
+  "duration": 86105800,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "BMW",
+      "offset": 15
+    }
+  ],
+  "location": "HomePageSteps.iSelectMake(String)"
+});
+formatter.result({
+  "duration": 164062500,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Any Model",
+      "offset": 16
+    }
+  ],
+  "location": "HomePageSteps.iSelectModel(String)"
+});
+formatter.result({
+  "duration": 198452900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "ACT-All",
+      "offset": 19
+    }
+  ],
+  "location": "HomePageSteps.iSelectLocation(String)"
+});
+formatter.result({
+  "duration": 107717800,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "$15,000",
+      "offset": 16
+    }
+  ],
+  "location": "HomePageSteps.iSelectPrice(String)"
+});
+formatter.result({
+  "duration": 115389700,
+  "status": "passed"
+});
+formatter.match({
+  "location": "HomePageSteps.iClickOnFindMyNextCarTab()"
+});
+formatter.result({
+  "duration": 5714661500,
+  "error_message": "org.openqa.selenium.WebDriverException: unknown error: session deleted because of page crash\nfrom unknown error: cannot determine loading status\nfrom tab crashed\n  (Session info: chrome\u003d101.0.4951.67)\nBuild info: version: \u00274.0.0\u0027, revision: \u00273a21814679\u0027\nSystem info: host: \u0027DESKTOP-CKALQP5\u0027, ip: \u0027192.168.1.177\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_121\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCommand: [ea60b7fb5ec29c1dcbae71ef24f6c854, findElement {using\u003dxpath, value\u003d//input[@id\u003d\u0027search-submit\u0027]}]\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 101.0.4951.67, chrome: {chromedriverVersion: 101.0.4951.41 (93c720db8323..., userDataDir: C:\\Users\\ThinkPad\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:59808}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), se:cdp: ws://localhost:59808/devtoo..., se:cdpVersion: 101.0.4951.67, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: ea60b7fb5ec29c1dcbae71ef24f6c854\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:200)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:133)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:53)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:184)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.invokeExecute(DriverCommandExecutor.java:164)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:139)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:559)\r\n\tat org.openqa.selenium.remote.ElementLocation$ElementFinder$2.findElement(ElementLocation.java:162)\r\n\tat org.openqa.selenium.remote.ElementLocation.findElement(ElementLocation.java:60)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:383)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:375)\r\n\tat org.openqa.selenium.support.pagefactory.DefaultElementLocator.findElement(DefaultElementLocator.java:70)\r\n\tat org.openqa.selenium.support.pagefactory.internal.LocatingElementHandler.invoke(LocatingElementHandler.java:39)\r\n\tat com.sun.proxy.$Proxy23.toString(Unknown Source)\r\n\tat au.com.carguide.pages.NewAndusedCarsearchPage.clickOnFindMyNextCar(NewAndusedCarsearchPage.java:62)\r\n\tat au.com.carguide.steps.HomePageSteps.iClickOnFindMyNextCarTab(HomePageSteps.java:37)\r\n\tat ✽.And I click on Find My Next Car tab(buy.feature:12)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "BMW",
+      "offset": 23
+    }
+  ],
+  "location": "HomePageSteps.iShouldSeeTheMakeInResults(String)"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.after({
+  "duration": 221407500,
+  "error_message": "org.openqa.selenium.NoSuchSessionException: invalid session id\nBuild info: version: \u00274.0.0\u0027, revision: \u00273a21814679\u0027\nSystem info: host: \u0027DESKTOP-CKALQP5\u0027, ip: \u0027192.168.1.177\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_121\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCommand: [ea60b7fb5ec29c1dcbae71ef24f6c854, screenshot {}]\nCapabilities {acceptInsecureCerts: false, browserName: chrome, browserVersion: 101.0.4951.67, chrome: {chromedriverVersion: 101.0.4951.41 (93c720db8323..., userDataDir: C:\\Users\\ThinkPad\\AppData\\L...}, goog:chromeOptions: {debuggerAddress: localhost:59808}, javascriptEnabled: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: WINDOWS, platformName: WINDOWS, proxy: Proxy(), se:cdp: ws://localhost:59808/devtoo..., se:cdpVersion: 101.0.4951.67, setWindowRect: true, strictFileInteractability: false, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unhandledPromptBehavior: dismiss and notify, webauthn:extension:credBlob: true, webauthn:extension:largeBlob: true, webauthn:virtualAuthenticators: true}\nSession ID: ea60b7fb5ec29c1dcbae71ef24f6c854\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.createException(W3CHttpResponseCodec.java:200)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:133)\r\n\tat org.openqa.selenium.remote.codec.w3c.W3CHttpResponseCodec.decode(W3CHttpResponseCodec.java:53)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:184)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.invokeExecute(DriverCommandExecutor.java:164)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:139)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:559)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:614)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:618)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.getScreenshotAs(RemoteWebDriver.java:349)\r\n\tat au.com.carguide.utility.Utility.getScreenshot(Utility.java:333)\r\n\tat au.com.carguide.Hooks.tearDown(Hooks.java:21)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke0(Native Method)\r\n\tat sun.reflect.NativeMethodAccessorImpl.invoke(NativeMethodAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingMethodAccessorImpl.invoke(DelegatingMethodAccessorImpl.java:43)\r\n\tat java.lang.reflect.Method.invoke(Method.java:498)\r\n\tat cucumber.runtime.Utils$1.call(Utils.java:40)\r\n\tat cucumber.runtime.Timeout.timeout(Timeout.java:16)\r\n\tat cucumber.runtime.Utils.invoke(Utils.java:34)\r\n\tat cucumber.runtime.java.JavaHookDefinition.execute(JavaHookDefinition.java:60)\r\n\tat cucumber.runtime.Runtime.runHookIfTagsMatch(Runtime.java:224)\r\n\tat cucumber.runtime.Runtime.runHooks(Runtime.java:212)\r\n\tat cucumber.runtime.Runtime.runAfterHooks(Runtime.java:206)\r\n\tat cucumber.runtime.model.CucumberScenario.run(CucumberScenario.java:46)\r\n\tat cucumber.runtime.junit.ExecutionUnitRunner.run(ExecutionUnitRunner.java:102)\r\n\tat org.junit.runners.Suite.runChild(Suite.java:128)\r\n\tat org.junit.runners.Suite.runChild(Suite.java:27)\r\n\tat org.junit.runners.ParentRunner$4.run(ParentRunner.java:331)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:79)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:329)\r\n\tat org.junit.runners.ParentRunner.access$100(ParentRunner.java:66)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)\r\n\tat org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:413)\r\n\tat cucumber.runtime.junit.ExamplesRunner.run(ExamplesRunner.java:59)\r\n\tat org.junit.runners.Suite.runChild(Suite.java:128)\r\n\tat org.junit.runners.Suite.runChild(Suite.java:27)\r\n\tat org.junit.runners.ParentRunner$4.run(ParentRunner.java:331)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:79)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:329)\r\n\tat org.junit.runners.ParentRunner.access$100(ParentRunner.java:66)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)\r\n\tat org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:413)\r\n\tat cucumber.runtime.junit.ScenarioOutlineRunner.run(ScenarioOutlineRunner.java:53)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:63)\r\n\tat cucumber.runtime.junit.FeatureRunner.runChild(FeatureRunner.java:18)\r\n\tat org.junit.runners.ParentRunner$4.run(ParentRunner.java:331)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:79)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:329)\r\n\tat org.junit.runners.ParentRunner.access$100(ParentRunner.java:66)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)\r\n\tat org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:413)\r\n\tat cucumber.runtime.junit.FeatureRunner.run(FeatureRunner.java:70)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:95)\r\n\tat cucumber.api.junit.Cucumber.runChild(Cucumber.java:38)\r\n\tat org.junit.runners.ParentRunner$4.run(ParentRunner.java:331)\r\n\tat org.junit.runners.ParentRunner$1.schedule(ParentRunner.java:79)\r\n\tat org.junit.runners.ParentRunner.runChildren(ParentRunner.java:329)\r\n\tat org.junit.runners.ParentRunner.access$100(ParentRunner.java:66)\r\n\tat org.junit.runners.ParentRunner$2.evaluate(ParentRunner.java:293)\r\n\tat org.junit.internal.runners.statements.RunAfters.evaluate(RunAfters.java:27)\r\n\tat org.junit.runners.ParentRunner$3.evaluate(ParentRunner.java:306)\r\n\tat org.junit.runners.ParentRunner.run(ParentRunner.java:413)\r\n\tat cucumber.api.junit.Cucumber.run(Cucumber.java:100)\r\n\tat org.junit.runner.JUnitCore.run(JUnitCore.java:137)\r\n\tat com.intellij.junit4.JUnit4IdeaTestRunner.startRunnerWithArgs(JUnit4IdeaTestRunner.java:69)\r\n\tat com.intellij.rt.junit.IdeaTestRunner$Repeater$1.execute(IdeaTestRunner.java:38)\r\n\tat com.intellij.rt.execution.junit.TestsRepeater.repeat(TestsRepeater.java:11)\r\n\tat com.intellij.rt.junit.IdeaTestRunner$Repeater.startRunnerWithArgs(IdeaTestRunner.java:35)\r\n\tat com.intellij.rt.junit.JUnitStarter.prepareStreamsAndStart(JUnitStarter.java:235)\r\n\tat com.intellij.rt.junit.JUnitStarter.main(JUnitStarter.java:54)\r\n",
+  "status": "failed"
+});
+});
